9f283782c0cba5d4d202fe28635fb9778bc92176
Yuliya Padzheva <yippee.smile@yandex.ru>
2022-06-26 17:06:43 +0300

    https

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -4,22 +4,22 @@ const AuthorisationError = require('../errors/AuthorisationError');
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, _res, next) => {
-    const { authorization } = req.headers;
+  const { authorization } = req.headers;
 
-    if (!authorization) {
-        next(new AuthorisationError('Необходима авторизация'));
-    } else {
-        const token = authorization.replace('Bearer ', '');
-        let payload;
+  if (!authorization) {
+    next(new AuthorisationError('Необходима авторизация'));
+  } else {
+    const token = authorization.replace('Bearer ', '');
+    let payload;
 
-        try {
-            payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
-        } catch (err) {
-            next(new AuthorisationError('Необходима авторизация'));
-            return;
-        }
-
-        req.user = payload;
+    try {
+      payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key');
+    } catch (err) {
+      next(new AuthorisationError('Необходима авторизация'));
+      return;
     }
-    next();
+
+    req.user = payload;
+  }
+  next();
 };
